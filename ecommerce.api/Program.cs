dbce3d849c2f2a5650d594ea8e3c62f928276abf
Vinicius Silva <vini.campos.silva@gmail.com>
2022-09-11 01:48:52 -0300

    mysql provider configurado

--- ecommerce.api/Program.cs
+++ ecommerce.api/Program.cs
@@ -1,3 +1,6 @@
+using ecommerce.infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string connectionString = builder.Configuration["ConnectionString"];
+
+#if DEBUG
+builder.Services.AddDbContext<EcommerceDbContext>(
+    dbContextOptions => dbContextOptions
+    .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+    .LogTo(Console.WriteLine, LogLevel.Information)
+    .EnableSensitiveDataLogging(true)
+    .EnableDetailedErrors(true)
+    );
+#else
+builder.Services.AddDbContext<EcommerceDbContext>(
+    dbContextOptions => dbContextOptions
+    .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+    );
+#endif
 
 var app = builder.Build();
 
